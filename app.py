f0a3a4b622e0d4f9504b731e175c52e85eb5786a
IqSantiagow <perkowskikrzysio@gmail.com>
2026-03-01 09:26:27 +0100

    Add Pydantic models and logging for journal events

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import logging
+
 from config.config import load_config
 from services.journal_watcher import JournalWatcherService
 
 config = load_config()
 
+log_level = config.ed.logging.level
+
+logging.basicConfig(
+    level= getattr(logging, log_level)
+)
+
 if __name__ == "__main__":
     print(config.ed.main_path)
     watcher = JournalWatcherService(config.ed.main_path)
-    for line in watcher.follow_journal():
-        print(line.strip())
+
+    for event in watcher.emit_journal_events():
+        print(event.event)
