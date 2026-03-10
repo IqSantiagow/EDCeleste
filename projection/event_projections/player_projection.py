9731b1e85fbe034d1be3c63d14e1fb8473beaa89
IqSantiagow <perkowskikrzysio@gmail.com>
2026-03-10 19:26:23 +0100

    Add PlayerProjection for managing player state and event processing

---
+++ projection/event_projections/player_projection.py
@@ -0,0 +1,32 @@
+import logging
+
+from pydantic import BaseModel
+
+from projection.event_projections.projection import Projection
+from services.models.game_events import LoadedGameEvent
+
+logger = logging.getLogger(__name__)
+
+
+class PlayerProjection(Projection):
+    PROJECTION_STRING = "Commander name is {0}. Commander has {1} of credits. Commander ship is {2}"
+
+    def __init__(self):
+        self.player_name = None
+        self.player_credits = 0
+        self.player_ship = None
+
+    def process_event(self, event: BaseModel):
+        if isinstance(event, LoadedGameEvent):
+            logger.debug("Received player state event: %s", event)
+            self.player_name = event.Commander
+            self.player_credits = event.Credits
+            self.player_ship = event.Ship
+            return
+
+    logger.debug("Received event but not withing allowed events. Skipping...")
+
+    def create_projection(self) -> str:
+        if not self.player_name or not self.player_ship:
+            logger.warning("Player state not set. Does the game started?")
+        return self.PROJECTION_STRING.format(self.player_name, self.player_credits, self.player_ship)
---
+++ tests/projection/test_player_projection.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import unittest
+
+from projection.event_projections.player_projection import PlayerProjection
+from services.models.game_events import LoadedGameEvent
+
+
+class PlayerProjectionTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.loaded_game_event = LoadedGameEvent(event="LoadGame", timestamp=datetime.now(), Commander="TestCommander",
+                                                FID="F123456", Horizons=True, Odyssey=False, Ship="Sidewinder",
+                                                ShipID=1, ShipIdent="TS-001", ShipName="Test Ship", StartLanded=False,
+                                                StartDead=False, GameMode="Solo", Group="", Credits=1000000, Loan=0,
+                                                FuelLevel=1.0, FuelCapacity=4.0, )
+
+    def test_should_process_event_and_set_player_state_data(self):
+        player_projection = PlayerProjection()
+
+        player_projection.process_event(self.loaded_game_event)
+
+        self.assertEqual(player_projection.player_name, self.loaded_game_event.Commander)
+        self.assertEqual(player_projection.player_credits, self.loaded_game_event.Credits)
+        self.assertEqual(player_projection.player_ship, self.loaded_game_event.Ship)
+
+
+    def test_should_create_projection(self):
+        player_projection = PlayerProjection()
+
+        player_projection.process_event(self.loaded_game_event)
+
+        expected_projection = "Commander name is {0}. Commander has {1} of credits. Commander ship is {2}".format(
+            self.loaded_game_event.Commander, self.loaded_game_event.Credits, self.loaded_game_event.Ship)
+
+        self.assertEqual(expected_projection, player_projection.create_projection())
+
